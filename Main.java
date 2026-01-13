18c31e4feff841410a558cd44535372f5fd09eef
TfueBr34 <samvit190@gmail.com>
2026-01-12 22:36:35 -0300

    tirando testes do terminal

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ public class Main {
         String caminho_saida = caminho_entrada.substring(0, pos) +"-out"+ caminho_entrada.substring(pos);
 
         if(args.length > 2){
-            System.err.println(args[2]);
             leitor l = new leitor(args[2]);
             String[] comandos = l.getLinhas(caminho_entrada);
             l.interpretador(comandos, caminho_saida);
