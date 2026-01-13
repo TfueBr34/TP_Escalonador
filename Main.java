5bf8168dd86124cd8eaac5baf2543735e93c66cb
TfueBr34 <samvit190@gmail.com>
2026-01-12 22:32:06 -0300

    Finalização Main

--- Main.java
+++ Main.java
@@ -1,17 +1,21 @@
-// import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
 
-        // System.out.println("Digite o caminho do arquivo de entrada seguido de seu nome");
         String caminho_entrada = args[0];
         int pos = caminho_entrada.lastIndexOf(".");
         String caminho_saida = caminho_entrada.substring(0, pos) +".out"+ caminho_entrada.substring(pos);
 
-        leitor l = new leitor(args[1]);
-        String[] comandos = l.getLinhas(caminho_entrada);
-        l.interpretador(comandos, caminho_saida);
-        // sc.close();
+        if(args.length > 2){
+            System.err.println(args[2]);
+            leitor l = new leitor(args[2]);
+            String[] comandos = l.getLinhas(caminho_entrada);
+            l.interpretador(comandos, caminho_saida);
+
+        }else{
+            leitor l = new leitor("0");
+            String[] comandos = l.getLinhas(caminho_entrada);
+            l.interpretador(comandos, caminho_saida);
+        }
+
     }
 }
